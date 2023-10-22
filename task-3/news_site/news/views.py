ac8be540691332b21ba120dcb47e510ec68f46cb
ArtemK64 <79468247+ArtemK64@users.noreply.github.com>
2023-10-22 14:02:34 +0400

    Added registration and login pages

---
+++ task-3/news_site/news/views.py
@@ -0,0 +1,102 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.core.mail import send_mail
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
+from django.shortcuts import render, get_object_or_404, redirect
+from taggit.models import Tag
+
+from news.forms import EmailPostForm, CommentForm
+from news.models import Post
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/news')
+    else:
+        form = UserCreationForm()
+    return render(request, 'news/registration/register.html', {'form': form})
+
+
+def login_for_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/news')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'news/registration/login.html', {'form': form})
+
+
+def post_list(request, tag_slug=None):
+    object_list = Post.objects.filter(status=Post.PostStatus.PUBLISHED)
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 5)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'news/post/list.html', {'page': page,
+                                                   'posts': posts,
+                                                   'tag': tag})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.objects.filter(status=Post.PostStatus.PUBLISHED).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request,
+                  'news/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@mynews.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'news/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
